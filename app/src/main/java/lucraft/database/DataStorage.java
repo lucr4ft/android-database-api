d18bcc18e1cbb40688502480e63e8f5df9c68b76
lucalewin <luca.lewin23@gmail.com>
2020-10-24 00:26:27 +0200

    created android library-project files

---
+++ app/src/main/java/lucraft/database/DataStorage.java
@@ -0,0 +1,17 @@
+package lucraft.database;
+
+public class DataStorage {
+
+    private static DataStorage instance;
+
+    //---------------------------------------
+
+    public static DataStorage getInstance() {
+        return instance;
+    }
+
+    public static void setInstance(String host, int port) {
+
+    }
+
+}
